5518098e1dbd3f141d17412c766ea3021b3739a6
David Muir Sharnoff <muir@idiom.org>
2022-04-22 20:04:19 -0700

    adjust how coverage is calculated to include top level

---
+++ fake_test.go
@@ -0,0 +1,11 @@
+package libschema_test
+
+import(
+	"testing"
+)
+
+// TestNothing exists so that there are at least some tests at the
+// libschema level. Without any tests, test coverage reports are not
+// generated.
+func TestNothing(t *testing.T) {
+}
