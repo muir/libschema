2b8e8558c6da5e758922cad173de1e5b4d3f39a4
David Muir Sharnoff <muir@idiom.org>
2022-04-28 19:54:08 -0700

    a tiny bit more coverage

--- logger_test.go
+++ logger_test.go
@@ -9,11 +9,20 @@ import (
 	"github.com/muir/testinglogur"
 )
 
+type p struct {
+	l interface{ Log(...interface{}) }
+}
+
+func (p p) Println(v ...interface{}) {
+	p.l.Log(v...)
+}
+
 // mostly this just needs to compile
 func TestLogFromThings(t *testing.T) {
-	_ = libschema.LogFromPrintln(log.Default())
+	_ = libschema.LogFromLogur(testinglogur.Get(t))
 	_ = libschema.LogFromLog(t)
-	log := libschema.LogFromLogur(testinglogur.Get(t))
+	_ = libschema.LogFromPrintln(log.Default())
+	log := libschema.LogFromPrintln(p{t})
 
 	log.Trace("trace")
 	log.Debug("debug")
